7b979a27504a22a8e49babfa7431f40f13b24fd7
Erick Naves <ericknevesea@gmail.com>
2026-01-16 14:20:32 -0300

    finish end point status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,32 @@
-import database from "infra/database.js"
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 ;");
-  response.status(200).json({chave : "bora de voleizin"});
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResults = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsResultsValue = databaseOpenedConnectionsResults.rows[0].count;
+  // console.log(databaseOpenedConnectionsResultsValue);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsResultsValue,
+      },
+    },
+  });
 }
 
 export default status;
