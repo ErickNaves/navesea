4d06865343f8a4687dfc77dba56ab95f4f0af5ec
Erick Naves <ericknevesea@gmail.com>
2026-01-19 16:45:33 -0300

    take off console log

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,12 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === 'development' ? false : true,
   });
-  console.log('Credenciais do Postgres:', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-  })
+  // console.log('Credenciais do Postgres:', {
+  //   host: process.env.POSTGRES_HOST,
+  //   port: process.env.POSTGRES_PORT,
+  //   user: process.env.POSTGRES_USER,
+  //   database: process.env.POSTGRES_DB,
+  // })
 
   try{
     await client.connect();
