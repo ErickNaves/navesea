5d68d83101dec41b3da8a7988d0a14ff827cc636
Erick Naves <ericknevesea@gmail.com>
2026-03-02 13:58:47 -0300

    prettier fix eslint.config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,14 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from 'eslint-config-next/core-web-vitals'
- 
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   {
     rules: {
-      'no-unused-vars': 'warn',
+      "no-unused-vars": "warn",
     },
   },
-  globalIgnores([
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
-])
- 
-export default eslintConfig
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
+
+export default eslintConfig;
